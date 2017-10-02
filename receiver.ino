1ec788c8117285d5d7512c56ca25713131c6ec62
Anders Kalhauge <anders@kalhauge.dk>
2017-10-02 12:08:17 +0200

    Indentation

--- receiver.ino
+++ receiver.ino
@@ -2,29 +2,23 @@
 #define CLK_IN 5
 
 void setup() {
-pinMode(DATA_IN, INPUT);
-pinMode(CLK_IN, INPUT);
-Serial.begin(9600);
-}
+  pinMode(DATA_IN, INPUT);
+  pinMode(CLK_IN, INPUT);
+  Serial.begin(9600);
+  }
 
 void loop() {
-if(ready()){
-  Serial.print(read());
-}
+  if (ready()) {
+    Serial.print(read());
+    }
+  }
 
-
-
-}
-
-int ready(){
-
+int ready() {
   return digitalRead(CLK_IN);
-}
+  }
 
-int read(){
-
+int read() {
   int bit = digitalRead(DATA_IN);
   while(digitalRead(CLK_IN));
   return bit;
-}
-
+  }
--- sender.ino
+++ sender.ino
@@ -1,33 +1,28 @@
 
 #define DATA_OUT 2
-#define CLK_OUT 3 
-#define FRAME 100
+#define CLK_OUT 3
+#define FRAME 10
 
 void setup() {
   pinMode(DATA_OUT, OUTPUT);
   pinMode(CLK_OUT, OUTPUT);
   Serial.begin(9600);
-}
-
-void loop() {
-  if(Serial.available()){
-    char c = Serial.read();
-    for(int i=0; i<8; i++){
-      send(c&1);
-      c = c >> 1;
-      
-   }
-   
   }
 
-}
+void loop() {
+  if (Serial.available()) {
+    char c = Serial.read();
+    for (int i = 0; i < 8; i++) {
+      send(c & 1);
+      c = c >> 1;
+      }
+    }
+  }
 
 void send (int bit) {
-  digitalWrite(DATA_OUT,bit);
-  digitalWrite(CLK_OUT,HIGH);
+  digitalWrite(DATA_OUT, bit);
+  digitalWrite(CLK_OUT, HIGH);
   delay(FRAME);
-  digitalWrite(CLK_OUT,LOW);
+  digitalWrite(CLK_OUT, LOW);
   delay(FRAME);
-  
-}
-
+  }
